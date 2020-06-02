3ab42329ce0aaf902479272522dfbc19da40cbb6
Siarhei Vaitsiuk <vaitsiuks@gmail.com>
2020-06-02 22:18:01 +0300

    Added render method and exported class SearchBar in SearhBar.js

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -11,8 +11,27 @@ class SearchBar extends React.Component {
     renderSortByOptions() {
         return Object.keys(sortByOptions).map(sortByOption => {
             const sortByOptionValue = sortByOptions[sortByOption];
-            return <li key={sortByOptionValue}>{sortByOption}</li>            
-        });
-
+            return <li key={sortByOptionValue}>{sortByOption}</li>
+        })
     }
-}
+    render() {
+        return (
+            <div Name="SearchBar">
+                <div className="SearchBar-sort-options">
+                    <ul>
+                        {this.renderSortByOptions()}
+                    </ul>
+                </div>
+                <div className="SearchBar-fields">
+                    <input placeholder="Search Businesses" />
+                    <input placeholder="Where?" />
+                </div>
+                <div className="SearchBar-submit">
+                    <a>Let's Go</a>
+                </div>
+            </div>
+        );
+    }
+};
+
+export default SearchBar;
