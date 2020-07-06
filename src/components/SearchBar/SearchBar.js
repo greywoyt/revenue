86f76ef80ca59fbdedbeb16583b85ef9c522d10b
Siarhei Vaitsiuk <vaitsiuks@gmail.com>
2020-07-06 21:01:18 +0300

    Added className to the <li> in render method

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -31,7 +31,7 @@ class SearchBar extends React.Component {
     renderSortByOptions() {
         return Object.keys(this.sortByOptions).map(sortByOption => {
             const sortByOptionValue = this.sortByOptions[sortByOption];
-            return <li key={sortByOptionValue}>{sortByOption}</li>
+            return <li key={sortByOptionValue} className = {getSortByClass(sortByOptionValue)}>{sortByOption}</li>
         })
     }
     render() {
