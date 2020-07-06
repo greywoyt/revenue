bc59652437929d91804cb72b9991780f3c4d207a
Siarhei Vaitsiuk <vaitsiuks@gmail.com>
2020-07-06 21:22:51 +0300

    Added onClick event to the  <li> in render method

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -6,8 +6,8 @@ class SearchBar extends React.Component {
         super(props);
         this.state = {
             term: '',
-            location: '' ,
-            sortBy: best_match
+            location: '',
+            sortBy: 'best_match'
         };
         this.sortByOptions = {
             'Best Match': 'best_match',
@@ -31,7 +31,9 @@ class SearchBar extends React.Component {
     renderSortByOptions() {
         return Object.keys(this.sortByOptions).map(sortByOption => {
             const sortByOptionValue = this.sortByOptions[sortByOption];
-            return <li key={sortByOptionValue} className = {getSortByClass(sortByOptionValue)}>{sortByOption}</li>
+            return <li key={sortByOptionValue} className = {this.getSortByClass(sortByOptionValue)} onClick = {this.handleSortByChange.bind(this, sortByOptionValue)}>
+                        {sortByOption}
+                    </li>
         })
     }
     render() {
