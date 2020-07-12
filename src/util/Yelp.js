ab65323152dee3c56ebfd418c2a99b0cad6dd4b3
Siarhei Vaitsiuk <vaitsiuks@gmail.com>
2020-07-12 18:52:24 +0300

    Completed search method in Yelp object

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -2,6 +2,35 @@ const apiKey = 'J9jz9eW-ilBU9oH39WblPrBSiMUo3vybJpM3BwdtcLDYnGf9-xJ2geTzhUuUWZZ8
 
 const Yelp = {
     search(term, location, sortBy) {
-        return featch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`)
+        const cors = 'https://cors-anywhere.herokuapp.com/';
+        const endPoinUrl =  'https://api.yelp.com/v3/businesses/search';
+        const urlToFeath = `${cors}${endPoinUrl}?term=${term}&location=${location}&sort_by=${sortBy}`;
+        const apiHeaders = {
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+            }
+        }
+        return featch(urlToFeath, apiHeaders).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if (jsonResponse.businesses) {
+               return jsonResponse.businesses.map(business => {
+                   return {
+                    id: business.id,
+                    imageSrc: business.image_url,
+                    name: business.name,
+                    address: business.location.address1,
+                    city: business.location.city,
+                    state: business.location.state,
+                    zipCode: business.location.zip_code,
+                    category: business.categories.title,
+                    rating: business.rating,
+                    reviewCount: business.review_count
+                   }
+               });
+            }
+        })
     }
 };
+
+export default Yelp;
